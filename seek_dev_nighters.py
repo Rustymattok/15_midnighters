40e14428511213da0066ce2863f7869b147c2e83
Rustymattok <you@example.com>
2020-01-15 01:53:30 +0300

    update the next:, once check for requests change logic for midnight insert defaultdict(list) update output logic

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,6 +1,7 @@
 import pytz
 import requests
 from datetime import datetime
+from collections import defaultdict
 
 
 def get_local_time(time_stamp, time_zone):
@@ -11,63 +12,63 @@ def get_local_time(time_stamp, time_zone):
 def get_midnight_attempts(attempts):
     midnight_attempts = []
     for attempt in attempts:
-        time_account = get_local_time(
-            attempt['timestamp'], attempt['timezone']
-        )
-        if 0 < time_account.hour < 5:
+        if is_midnight_attempt(attempt):
             midnight_attempts.append(attempt)
     return midnight_attempts
 
 
+def is_midnight_attempt(attempt):
+    time_account = get_local_time(attempt['timestamp'], attempt['timezone'])
+    if 0 < time_account.hour < 5 and not None:
+        return True
+    return False
+
+
 def load_attempts(link_api):
     page = 0
+    number_of_pages = requests.get(link_api).json()['number_of_pages']
     while True:
         page = page + 1
+        if page > number_of_pages:
+            return False
         payload = {'page': page}
         accounts_page = requests.get(link_api, params=payload)
-        if not accounts_page.ok:
-            return False
         attempts = accounts_page.json()['records']
         midnight_attempts = get_midnight_attempts(attempts)
         yield from midnight_attempts
 
 
 def get_users_attempts(midnight_attempts):
-    user_attempts = dict()
-    key_time = 0
+    user_attempts = defaultdict(list)
     for midnight_attempt in midnight_attempts:
-        if midnight_attempt['username'] in user_attempts.keys():
-            user_attempts[midnight_attempt['username']][key_time].\
-                append(str(midnight_attempt['timestamp']))
-        else:
-            user_attempts[midnight_attempt['username']] = [
-                [str(midnight_attempt['timestamp'])],
-                str(midnight_attempt['timezone'])
-            ]
+        user_attempts[midnight_attempt['username']].append(
+            (midnight_attempt['timezone'], midnight_attempt['timestamp'])
+        )
     return user_attempts
 
 
 def output_midnighters(midnight_attempts):
     fmt = '%Y-%m-%d %H:%M:%S %Z%z'
     user_attempts = get_users_attempts(midnight_attempts)
-    key_time = 0
-    key_time_zone = 1
-    for user_name in user_attempts:
-        print('midnight_attempt account: ', user_name)
-        for time_attempt in user_attempts[user_name][key_time]:
-            time_zone = user_attempts[user_name][key_time_zone]
-            time_posted = get_local_time(float(time_attempt), time_zone).\
-                strftime(fmt)
-            path_file_out = 'total time_posted:{time_posted}'. \
+    key_username = 0
+    key_time = 1
+    for user in user_attempts.items():
+        print('midnight_attempt account: ', user[key_username])
+        for time_zone, time_stamp in user[key_time]:
+            time_posted = get_local_time(time_stamp, time_zone).strftime(fmt)
+            out_put_time = 'total time_posted:{time_posted}'.\
                 format(time_posted=time_posted)
-            print(path_file_out)
+            print(out_put_time)
 
 
 def main():
     link = 'http://devman.org/api/challenges/solution_attempts'
     if not requests.get(link).ok:
         print('not correct link')
-    output_midnighters(load_attempts(link))
+    try:
+        output_midnighters(get_midnight_attempts(load_attempts(link)))
+    except ValueError:
+        print('no json data')
 
 
 if __name__ == '__main__':
